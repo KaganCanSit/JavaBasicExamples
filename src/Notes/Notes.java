94b053bfc0884612a22609f1f6550dedde333f02
Kağan Can Şit <kagancansit@hotmail.com>
2021-12-05 13:29:39 +0300

    05Dec21/FirstCommit

---
+++ src/Notes/Notes.java
@@ -0,0 +1,15 @@
+package Notes;
+
+public class Notes {
+	
+/*
+	NOTES
+	- Tum kodlar Java Eclipse uzerinden yazildi. Fakat baska IDE'lerde kullanilabilir. IntelliJ gibi.
+	- Eclipse IDE ek yazilim bilesenleri ile genisletilebilir. Buna plug_in adi verilir.
+	
+*/	
+	
+	
+	
+	
+}
