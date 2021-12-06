fb1ce28c509ad6141071efbc3a808796252fdf09
Kağan Can Şit <kagancansit@hotmail.com>
2021-12-06 14:49:00 +0300

    06Dec21

---
+++ src/StringMethods_Sample19/IndexOf.java
@@ -0,0 +1,16 @@
+package StringMethods_Sample19;
+
+public class IndexOf {
+	public static void main(String[] args) {
+		//indexof() > Bir yerden sonrasini kontrol et, bir karakterden sonrasini kontrol et. > mail kontrolu gibi
+		String Str = new String("Welcome to Tutorials about Java");
+		String SubStr1 = new String("Tutorials");
+		String SubStr2 = new String("Sutorials");
+		
+		System.out.println("\nFound Index: " + Str.indexOf('o')); // o karakterinin indexini don
+		System.out.println("Found Index but Five Later: " + Str.indexOf('o',5));	//O harfi 5.indisten sonra varsa o indisi dön
+		System.out.println("Found String Find My String: " + Str.indexOf(SubStr1));	// Aradigim string ifade icerisinde var mi?
+		System.out.println("Found String Find My String Later 5 Chracters: " + Str.indexOf(SubStr1,5));	// Aradigim ifade string icerisinde 5.index'ten sonra var mi?
+		System.out.println("Found Index: " + Str.indexOf(SubStr2));
+	}
+}
---
+++ src/StringMethods_Sample19/StartsWidth.java
@@ -0,0 +1,11 @@
+package StringMethods_Sample19;
+
+public class StartsWidth {
+	public static void main(String[] args) {
+		String Str = new String("Welcome to Tutorials");
+		
+		System.out.println("Return Value: " + Str.startsWith("Welcome"));		//Str Welcome ile basliyor mu?
+		System.out.println("Return Value: " + Str.startsWith("Tutorials"));		//Str Tutorial ile basliyor mu?
+		System.out.println("Return Value: " + Str.startsWith("Tutorials",11));	//Str 11.indisten itibaren Tutorials ile basliyor mu?
+	}
+}
